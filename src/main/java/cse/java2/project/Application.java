decc583f6bc5d94beb0b111eefa9ff9f0ed05374
Evan Li <evan3li@outlook.com>
2023-05-05 20:16:11 +0800

    Initial commit

---
+++ src/main/java/cse/java2/project/Application.java
@@ -0,0 +1,17 @@
+package cse.java2.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the main class of the Spring Boot application.
+ * mvn spring-boot:run
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
